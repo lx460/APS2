db2d88afff81022bff0bb9b2e501182fbf1ce9fa
lx460 <32945436+lx460@users.noreply.github.com>
2023-02-14 22:00:45 +0900

    Create q80_boj13251_조약돌꺼내기.java

---
+++ 10-1_조합/q80_boj13251_조약돌꺼내기.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class q80_boj13251_조약돌꺼내기 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int M = Integer.parseInt(br.readLine());
+		double[] stone = new double[M + 1];
+		double[] percent = new double[M + 1];
+		double sum = 0;
+		double ans = 0.0;
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 1; i <= M; i++) {
+			stone[i] = Double.parseDouble(st.nextToken());
+			sum += stone[i];
+		}
+		int K = Integer.parseInt(br.readLine());
+		for (int i = 1; i <= M; i++) {
+			int cnt = 0;
+			if (stone[i] >= K) {
+				percent[i] = 1.0;
+				for (int j = 0; j < K; j++) {
+					percent[i] *= (stone[i] - j) / (sum - j);
+				}
+			}
+			ans += percent[i];
+
+		}
+		System.out.println(ans);
+
+	}
+}
